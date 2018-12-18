55d1eb4e01486395c8a0df7452e2bf5599a24489
kitrak18 <43020982+kitrak18@users.noreply.github.com>
2018-12-17 23:24:57 -0500

    button

--- uiTry.java
+++ uiTry.java
@@ -3,9 +3,14 @@ class uiTry{
     public static void main(String args[]){
        JFrame frame = new JFrame("My First GUI");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.setSize(300,300);
-       JButton button = new JButton("Press");
+       frame.setSize(300,600); // frame size
+       JButton button = new JButton("Button1"); // creating button
        frame.getContentPane().add(button); // Adds Button to content pane of frame
+       
+       JButton button1 = new JButton("b2");
+       frame.getContentPane().add(button1); // Adds Button to content pane of frame
+       
+       // settting the frame to be visible
        frame.setVisible(true);
     }
 }
